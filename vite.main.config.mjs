64cffe45572ffb002aed7698e39038ad3111ddbe
Takunda Kabvunye <kabvunyet@gmail.com>
2026-02-13 09:16:40 +0200

    feat: update electron-updater version and adjust Vite configuration for external dependencies

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['sqlite3', 'electron'],
+      external: ['sqlite3', 'electron', 'electron-updater'],
       output: {
         format: 'cjs'
       }
     }
   },
   optimizeDeps: {
-    exclude: ['sqlite3', 'electron']
+    exclude: ['sqlite3', 'electron', 'electron-updater']
   }
 });
